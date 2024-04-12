01d847f08763ccbf55e65047185bcdfc733956c6
paryanial888 <paryanial888@northwested.org>
2024-04-12 13:23:14 -0400

    Update main.js

--- 007 1.4/main.js
+++ 007 1.4/main.js
@@ -8,12 +8,12 @@ document.getElementById("btn").addEventListener("click", placeOrder);
 // Store order code in a function
 function placeOrder() {
     // INPUT
-    let size = prompt("Enter size of pizza (small, medium, large): ");
-    let topping1 = prompt("Enter first topping: ");
-    let topping2 = prompt("Enter second topping: ");
+    const size = prompt("Enter size of pizza (small, medium, large): ");
+    const topping1 = prompt("Enter first topping: ");
+    const topping2 = prompt("Enter second topping: ");
 
     // PROCESS
-    let msg = `Your ${size} pizza with ${topping1} and ${topping2} will be ready soon!`;
+    const msg = `Your ${size} pizza with ${topping1} and ${topping2} will be ready soon!`;
 
     // OUTPUT
     alert(msg);
